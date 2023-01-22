a90c11f5910296d56eabbfe40ac447df9d2f4512
ktelegenov <ktelegenov@gmail.com>
2023-01-22 13:52:16 +0300

    added weather.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-// import { useEffect, useState } from "react";
-// import "semantic-ui-css/semantic.min.css";
-// import Weather from "./Weather";
+import { useEffect, useState } from "react";
+import "semantic-ui-css/semantic.min.css";
+import Weather from "./components/Weather";
 
 function App() {
   // const [lat, setLat] = useState([]);
   // const [long, setLong] = useState([]);
 
-  // const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
   // const [city, setCity] = useState("London");
 
-  // const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=5a139e2949d094a8d498f0bc4fdc8099`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=5a139e2949d094a8d498f0bc4fdc8099`;
 
-  // useEffect(() => {
-  //   fetch(apiUrl)
-  //     .then((res) => res.json())
-  //     .then((result) => {
-  //       setData(result);
-  //       console.log(result);
-  //     });
-  // }, [apiUrl]);
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result);
+        console.log(result);
+      });
+  }, [apiUrl]);
 
   
   return (
@@ -29,7 +29,7 @@ function App() {
         <input></input>
       </form>
 
-      {/* <Weather weatherData={data} /> */}
+      <Weather weatherData={data} />
     </div>
   );
 }
