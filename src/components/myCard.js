72f1e0c9860719688a35f3844589c033045f6fa3
ktelegenov <ktelegenov@gmail.com>
2023-01-31 10:01:39 +0300

    changes in the card

--- src/components/myCard.js
+++ src/components/myCard.js
@@ -8,14 +8,13 @@ function MyCard(props) {
     return (
       <div>
         <Card border="info" style={{ width: "18rem" }}>
-          <Card.Header>Weather results</Card.Header>
+          <Card.Header>{props.info.name}</Card.Header>
           <Card.Body>
-            <Card.Title>{props.info.name}</Card.Title>
+            <Card.Title>{props.info.weather[0].description}</Card.Title>
             <Card.Text>
               <p>Temperature: {Math.round(props.info.main.temp - 273.15)} C </p>
               <p>Feels like: {Math.round(props.info.main.feels_like - 273.15)} C</p>
-              <p>Humidity: {props.info.main.humidity} </p>
-              <p>Description: {props.info.weather[0].description}</p>
+              <p>Humidity: {props.info.main.humidity} </p>              
             </Card.Text>
           </Card.Body>
         </Card>
